a108dd0b7f9bc8668220a506347b6fad8f3943d4
John Morgan <johnmorgan@uk.ibm.com>
2015-11-02 23:07:21 +0000

    calling random item from array

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-//when button is submitted - check if there is an answer in the field
-
-//if not create random number and bull from the blank field array
-
-//else create random number and pull from answer array
-
-//display answer in text field
-
 var usersIdea = prompt("tell me your idea!");
 
+var data = [
+"Hello",
+"Yo Yo",
+"Bonjour",
+"Howdy"
+];
+
 function randomNumberFunction(upper) {
-  	return Math.floor( (Math.random() * upper) + 1);
+  	return Math.floor( (Math.random() * upper) );
 }
 
 function thisGuysAnswer() {
-if (usersIdea === "") {
-	alert("You havent told me your idea?");
-} else {
-	var randomNumber = randomNumberFunction;
-	alert("Sik idea brah! " + randomNumber(10));
-	};
-}
+	var randomNumber = randomNumberFunction(data.length);
+	return data[randomNumber];
+};
 
-thisGuysAnswer();
+alert( thisGuysAnswer() );
+
+
+
+
+
+
