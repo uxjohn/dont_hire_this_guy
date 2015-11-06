e10ff8a435e871b09464585fff197cb138bd69c5
John Morgan <johnmorgan@uk.ibm.com>
2015-11-06 12:53:54 +0000

    making changes in the dom

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var usersIdea = prompt("tell me your idea!");
+document.getElementById('response').innerHTML = "Hi, I am kind of a big deal around here.  Tell me your idea, lets see how innovative you are!";
 
+//Create random number
 function randomNumberFunction(upper) {
   	return Math.floor( (Math.random() * upper) );
-}
-
-function thisGuysAnswer() {
-	var randomNumber = randomNumberFunction(data.length);
-	return data[randomNumber];
 };
 
-document.write( "<p>" + thisGuysAnswer() + "</p>");
+//The charicters random answer
+function thisGuysAnswer() {
+	var randomNumber = randomNumberFunction(data.length);
+	var idea = document.getElementById("idea").value;
 
-
-
-
-
-
-
-
+	if (idea === "") {
+		document.getElementById('response').innerHTML = "You need to tell me your idea";
+	} else {
+		document.getElementById('response').innerHTML = data[randomNumber];
+	};
+	
+};
